00e76c6b9d7c4e9adb3809370013703edccbd5db
Igor <37709632+Tempesto@users.noreply.github.com>
2025-02-16 08:55:30 +0000

    Add action, docker and entrypoints files

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Specify the base image for the container. In this case, the latest version of Ubuntu is used.
+FROM ubuntu:latest
+
+# Update the package manager and install necessary packages:
+# - python3.12: Python interpreter version 3.12
+# - python3-pip: Python package manager
+# - git: version control system
+RUN apt-get update && apt-get install -y \
+    python3.12 \
+    python3-pip \
+    git
+
+# Install the PyYAML library using pip3 (Python package manager).
+RUN pip3 install PyYAML
+
+# Copy the `feed.py` file from the local filesystem into the container, placing it at `/usr/bin/feed.py`.
+COPY feed.py /usr/bin/feed.py
+
+# Copy the `entrypoint.sh` file from the local filesystem into the root of the container.
+COPY entrypoint.sh /entrypoint.sh
+
+# Specify that the `/entrypoint.sh` script should be executed when the container starts.
+# This script will become the main process of the container.
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Print a separator line for better readability in logs.
+echo "======================"
+
+# Configure the global Git username using the environment variable ${GITHUB_ACTOR}.
+# This sets the name of the user running the script.
+git config --global user.name "${GITHUB_ACTOR}"
+
+# Configure the global Git email using the environment variable ${INPUT_EMAIL}.
+# This sets the email address associated with Git commits.
+git config --global user.email "${INPUT_EMAIL}"
+
+# Add the /github/workspace directory as a safe directory in Git.
+# This is necessary to avoid Git security warnings when working in certain environments.
+git config --global --add safe.directory /github/workspace
+
+# Run a Python script located at /usr/bin/feeed.py.
+# Note: There is a typo in the script name ("feeed.py" should likely be "feed.py").
+python3 /usr/bin/feeed.py
+
+# Stage all changes in the Git repository and create a commit with the message "Update Feed".
+git add -A && git commit -m "Update Feed"
+
+# Push the changes to the remote repository's `main` branch.
+# The `--set-upstream` flag ensures the local branch tracks the remote branch.
+git push --set-upstream origin main
+
+# Print another separator line for better readability in logs.
+echo "======================"
